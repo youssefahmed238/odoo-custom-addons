b6d22b7b49c1ac4e4508c65240ea37c960f5d9f7
Youssef Ahmed <youssef01120646586@gmail.com>
2025-08-11 16:55:58 +0300

    create a manifest file

---
+++ hr_payslips_custom_excel/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "HR Payslips Custom Excel",
+    "author": "Youssef",
+    "category": "Human Resources",
+    "version": "17.0.0.1.0",
+    "depends": ["hr_payroll"],
+    "data": [],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
