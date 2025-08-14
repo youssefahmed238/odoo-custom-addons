c6e5b70b18c1630859b60115c3c13ce21ae21725
Youssef Ahmed <youssef01120646586@gmail.com>
2025-08-14 14:11:35 +0300

    initiate manifest

---
+++ link_payment_and_shipping_methods_in_website/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Link Payment and Shipping Methods in Website',
+    'author': 'Youssef',
+    'category': 'Website',
+    'version': '17.0.1.0.0',
+    'depends': ['website_sale', 'payment', 'delivery'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
