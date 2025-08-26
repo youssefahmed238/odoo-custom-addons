5506a9854a0b05263aff0b010eebbb35f7382617
Youssef Ahmed <youssef01120646586@gmail.com>
2025-08-26 15:58:28 +0300

    initiate manifest file

---
+++ documents_delete_control/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Documents Delete Control',
+    'author': 'Youssef',
+    'category': 'Productivity',
+    'version': '17.0.0.1.0',
+    'depends': ['documents'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
